127c1d446ce19eeb2033b1bb717db38ca6757660
Scott Hurt <scotthurt@MacBook-Pro-55.local>
2019-11-04 13:49:06 -0600

    Added in the list of quotes. Set up the QuoteBox component to use quote props passed in. Added functionality to generate and show a new random quote

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React, {Component} from 'react';
 
 import './App.css';
 import QuoteBox from './QuoteBox';
+import quotes from './quotes';
 
 class App extends Component {
+
+  //Setting up constructor functions so we can set our default state
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+  }
+
+  //Create a function called getRandomQuote that returns a random quote from the array of quotes.  Hint: You'll need to generate a random number between  0 and 1 less than the length of the array. 
+  getRandomQuote() {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    return quotes [randomQuoteIndex];
+  }
+
+  //Function that will be called when the new quote button on the QuoteBox component is pressed
+  handleClick = () => {
+    //Generate a random quote 
+    const randomQuote = this.getRandomQuote();
+    //Update our state with the info from that random quote
+    this.setState({
+      quote:randomQuote.quote,
+      author:randomQuote.author
+    });
+  }
+  
   render() {
-    return (
+   return (
+
+    
     <div>
-      <QuoteBox />
+      <QuoteBox 
+      quote= {this.state.quote}
+      author= {this.state.author}
+      handleClick={this.handleClick}
+      />
     </div>
   );
 }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,83 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+
 
 class QuoteBox extends Component {
     render() {
         return (
             <div> 
-               <h1>Hi</h1>
+               <h1>{this.props.quote}</h1>
+               <h1>{this.props.author}</h1>
+               <button onClick={this.props.handleClick}>New Quote</button>
             </div>
         );
     }
 }
-export default QuoteBox;
 
-    //Setting up our Initial State 
-    //The constructor function gets called whenever we make a new CowClicker Component
     
-        //Super refers to the class we are extending - in this case -, we also want to call the constructor for Component so that any additional setup gets done
-        super (props);
-        //Set up our state object
-        this.state = { clicks: 0 };
-   
-
-   
-
-        //Don't Do this 
-        //this.state = { clicks: this.state.clicks + 1 }; 
-
-    }
-
-    import React, { Component } from 'react';
-
-    class CowClicker extends Component {
-    
-        //Setting up our Initial State 
-        //The constructor function gets called whenever we make a new CowClicker Component
-        constructor(props) {
-            //Super refers to the class we are extending - in this case -, we also want to call the constructor for Component so that any additional setup gets done
-            super (props);
-            //Set up our state object
-            this.state = { clicks: 0 };
-       
-    
-        }
-        onCowClick = () => {
-            console.log("Cow Clicked");
-            this.setState({
-                clicks: this.state.clicks + 1
-    
-            });
-    
-            //Don't Do this 
-            //this.state = { clicks: this.state.clicks + 1 }; 
-    
-        }
-    
-        componentWillMount() {
-            console.log ("Component is getting ready to mount");
-    
-        }
-        componentDidMount(){
-            console.log ("Component has finished mounting");
-        }
-    
-        componentWillUpdate() {
-            console.log ("Component is finished mounting");
-    
-        }
-        componentDidUpdate() {
-            console.log("Component has finished updating");
-        }
-        render() {
-            console.log("Render Called");
-            return (
-                <div>
-                <div> Clicks: {this.state.clicks}</div>
-                <img src= 'http://s3.bypaulshen.com.s3-us-west-2.amazonaws.com/buildwithreact/cow.png' onClick= {this.onCowClick} alt="Cow" />
-                <p> Click the cow</p>
-                </div>
-            );
-        }
-    }
-    export default CowClicker;
+    export default QuoteBox;
