c92f3e47c991eb3bffc0df4ccf6324ebbb7013d6
Scott Hurt <scotthurt@MacBook-Pro-55.local>
2019-11-04 14:31:42 -0600

    Added the function to generate  a random color. Set up the QuoteBox component with some styling.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,18 @@ class App extends Component {
       author:randomQuote.author
     });
   }
+
+  //Function to generate a random color from R: 0-154, G: 0-154, B: 0-154
+  getRandomColor(upperBound)  {
+    const color = `rgb(
+     ${Math.floor(Math.random() * upperBound)},
+     ${Math.floor(Math.random() * upperBound)},
+     ${Math.floor(Math.random() * upperBound)}
+
+    )`;
+    return color;
+
+  }
   
   render() {
    return (
@@ -38,6 +50,7 @@ class App extends Component {
     
     <div>
       <QuoteBox 
+      color={this.getRandomColor(155)}
       quote= {this.state.quote}
       author= {this.state.author}
       handleClick={this.handleClick}
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 
 class QuoteBox extends Component {
     render() {
+
+        //Set the backgrouind color ofnthe HTML element to the random color that is passed in as a prop
+
+        const html = document.documentElement;
+        html.style.backgroundColor = this.props.color;
+
         return (
-            <div> 
+            <div style= {{backgroundColor: 'white'}}>
                <h1>{this.props.quote}</h1>
+               <h4 style={{ color: this.props.color}}>
+                   -{this.props.author !== '' ? this.props.author :
+                   'Unknown'}-
+               </h4>
+               <button>
+                   style={{ backgroundColor:this.props.color}}
+                   
+               </button>
                <h1>{this.props.author}</h1>
                <button onClick={this.props.handleClick}>New Quote</button>
             </div>
